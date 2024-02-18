df8163debde81686954f749fe301c978c1a6f6b8
szeleszsolt <szeles.zsolt@blathy.info>
2024-02-18 17:22:17 +0100

    commit

---
+++ musiclibraryprog/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musiclibraryprog
+{
+    internal class Music
+    {
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public DateTime Release { get; set; }
+        public string? Style { get; set; }
+    }
+}
