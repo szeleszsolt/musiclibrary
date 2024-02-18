051fbf0e0ae7aa42e655a1008f9a02b34a248550
szeleszsolt <szeles.zsolt@blathy.info>
2024-02-18 18:18:46 +0100

    MusicEditorWindowSO

--- musiclibraryprog/MusicEditorWindow.xaml.cs
+++ musiclibraryprog/MusicEditorWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace musiclibraryprog
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MusicEditorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MusicEditorWindow : Window
     {
-        public MainWindow()
+        public MusicEditorWindow()
         {
             InitializeComponent();
-        }
-
-        private void MusicPlus(object sender, RoutedEventArgs e)
-        {
-            MusicEditorWindow window = new MusicEditorWindow();
-            window.ShowDialog();
         }
     }
 }
